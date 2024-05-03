56f759ee94ec5319a561153b41b3f959586a69b1
WardDeb <ward@deboutte.be>
2024-05-03 17:27:19 +0200

    accomodate noInt in the coef puller

--- rmd/funcs.R
+++ rmd/funcs.R
@@ -1,15 +1,18 @@
-coeflist_to_ggplot <- function(dss, geneid) {
+coeflist_to_ggplot <- function(dss, geneid, int=TRUE) {
   # Get coefficients and subset for the wanted gene in a df
   tdf <- coef(dds) %>%
     as.data.frame %>%
     dplyr::filter(row.names(dds) == geneid)
-  # extract intercept and keep for later
-  icept <- tdf$Intercept
-  # Sum up all columns with intercept
-  tdf <- tdf + icept
-  # Restore initial intercept value
-  tdf$Intercept <- icept
-  # melt
-  tdf <- tdf %>% pivot_longer( colnames(tdf))
-  return(tdf)
+  if (int==TRUE){
+    # extract intercept and keep for later
+    icept <- tdf$Intercept
+    # Sum up all columns with intercept
+    tdf <- tdf + icept
+    # Restore initial intercept value
+    tdf$Intercept <- icept
+    # melt
+    tdf <- tdf %>% pivot_longer( colnames(tdf))
+    return(tdf)
+  }
+  return(tdf %>% pivot_longer(colnames(tdf)))
 }
